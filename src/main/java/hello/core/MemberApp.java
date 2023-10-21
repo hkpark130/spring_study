4409436ef53a40e6d29a05b0947e59f50565e8ea
hkpark130 <hkpark130@naver.com>
2023-10-21 16:02:43 +0900

    4/2

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -7,6 +7,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
@@ -19,9 +21,11 @@ public class MemberApp {
 //        Member findMember = memberService.findMember(1L);
 //        System.out.println("new member = " + member.getName());
 //        ----------------
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // 스프링 컨테이너(인터페이스)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
@@ -29,6 +33,7 @@ public class MemberApp {
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         System.out.println("new order = " + order);
+
 
     }
 }
